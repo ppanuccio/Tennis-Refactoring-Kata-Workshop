23d4bf7eb00b28e93855db0a66572494892ed850
Pietro Di Bello <pierodibello@betelgeuse.homenet.telecomitalia.it>
2016-10-24 16:20:48 +0200

    Remove Tennis Game 2 and 3

--- java/src/main/java/TennisGame.java
+++ java/src/main/java/TennisGame.java
@@ -1,6 +1,6 @@
 
 public class TennisGame {
-
+    
     private int m_score1 = 0;
     private int m_score2 = 0;
     private String player1Name;
@@ -12,11 +12,10 @@ public class TennisGame {
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1") {
+        if (playerName == "player1")
             m_score1 += 1;
-        } else {
+        else
             m_score2 += 1;
-        }
     }
 
     public String getScore() {
@@ -27,40 +26,34 @@ public class TennisGame {
             switch (m_score1)
             {
                 case 0:
-                    score = "Love-All";
+                        score = "Love-All";
                     break;
                 case 1:
-                    score = "Fifteen-All";
+                        score = "Fifteen-All";
                     break;
                 case 2:
-                    score = "Thirty-All";
+                        score = "Thirty-All";
                     break;
                 default:
-                    score = "Deuce";
+                        score = "Deuce";
                     break;
-
+                
             }
         }
         else if (m_score1>=4 || m_score2>=4)
         {
             int minusResult = m_score1-m_score2;
-            if (minusResult==1) {
-                score ="Advantage player1";
-            } else if (minusResult ==-1) {
-                score ="Advantage player2";
-            } else if (minusResult>=2) {
-                score = "Win for player1";
-            } else {
-                score ="Win for player2";
-            }
+            if (minusResult==1) score ="Advantage player1";
+            else if (minusResult ==-1) score ="Advantage player2";
+            else if (minusResult>=2) score = "Win for player1";
+            else score ="Win for player2";
         }
         else
         {
             for (int i=1; i<3; i++)
             {
-                if (i==1) {
-                    tempScore = m_score1;
-                } else { score+="-"; tempScore = m_score2;}
+                if (i==1) tempScore = m_score1;
+                else { score+="-"; tempScore = m_score2;}
                 switch(tempScore)
                 {
                     case 0:
